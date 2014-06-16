e19612093f639c60a70882006c854261c105b255
nic <acer@hdb3.net>
2014-06-16 20:39:54 +0100

    add framework main and forkIO for fraework thread

--- Framework.hs
+++ Framework.hs
@@ -1,9 +1,26 @@
-module Framework where
 
+import System.Environment(getArgs)
+import System.IO
+import System.Exit
+import qualified Data.Map as M
+import Control.Concurrent
 import Message
-import ParseConfig
+import Config
 
-framework configData = do
-    configItems = parseConfig configData
-    linkMap = 
-    routerMap = mapM
+framework routers links = do
+    putStrLn "Hello from Framework"
+    putStrLn $ "I have " ++ show (length routers) ++ " routers and " ++ show (M.size links) ++ " links."
+
+
+main = do
+    args <- getArgs
+    let arg1 = if null args then "config.txt" else args !! 0
+    putStrLn $ "Using file " ++ arg1 ++ " for configuration"
+    (routers,links) <- fileConfig arg1
+    frameworkThread <- forkIO (framework routers links)
+    hSetBuffering stdin NoBuffering
+    consoleLoop
+
+consoleLoop = do
+    c <- getChar
+    if 'q' == c then exitSuccess else consoleLoop
