c3592c1db8c09aa300f84ea061fe21c8eeb390e4
nic <acer@hdb3.net>
2014-06-20 22:07:00 +0100

    abstracting network addresses

---
+++ NetAddress.hs
@@ -0,0 +1,12 @@
+module NetAddress where
+
+{-
+  this is an abstraction of network address schemes to ensure that no unwarranted assumptions are made
+  regarding the semantics of network addresses
+
+  the additional consideration of multiple types of address is not considered, nor yet is explicit support for
+  host addresses, multicast, variable length subnets, or even subnets at all.....
+
+-}
+
+newtype NetAddress = NetAddress Int deriving (Show,Eq)
--- NetMessage.hs
+++ NetMessage.hs
@@ -1,6 +1,7 @@
 module NetMessage where
 
+import NetAddress
 import DVPMessage
 data NetMessage = EchoNM EchoMsg | DVPNM DVPMsg | InfoNM String deriving Show
 
-data EchoMsg = EchoReq Int Int | EchoRsp Int Int | EchoND Int Int Int deriving Show
+data EchoMsg = EchoReq NetAddress NetAddress | EchoRsp NetAddress NetAddress | EchoND NetAddress NetAddress NetAddress deriving Show
